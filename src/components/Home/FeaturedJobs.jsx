b64987520af4d1a01fccbb35925961d0ca10f758
dev-rahim <rahimuddin5915@gmail.com>
2023-04-12 14:11:56 +0600

    featured jobs show all jobs button added

--- src/components/Home/FeaturedJobs.jsx
+++ src/components/Home/FeaturedJobs.jsx
@@ -3,11 +3,15 @@ import FeaturedSingleJobs from "./FeaturedSingleJobs";
 
 const FeaturedJobs = () => {
   const [jobs, setJobs] = useState([]);
+  const [length, setLength] = useState(4);
   useEffect(() => {
     fetch("featuredJobs.json")
       .then((res) => res.json())
       .then((data) => setJobs(data));
   }, []);
+  const handleShowAll = () => {
+    setLength(jobs.length);
+  };
   return (
     <div className="text-center">
       <h2 className="text-5xl">Featured Jobs</h2>
@@ -16,10 +20,18 @@ const FeaturedJobs = () => {
         need. Its your future
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-        {jobs.map((job) => (
+        {jobs.slice(0, length).map((job) => (
           <FeaturedSingleJobs key={job.id} job={job} />
         ))}
       </div>
+      {length == 4 && (
+        <button
+          onClick={handleShowAll}
+          className=" btn btn-bg-gradient border-0"
+        >
+          See All Jobs
+        </button>
+      )}
     </div>
   );
 };
